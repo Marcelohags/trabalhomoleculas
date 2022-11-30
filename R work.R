91f01fe4c815e561c183a8a843bc0d11f47db0ec
Marcelohags <marcelohenrique.araujo@Hotmail.com>
2022-11-30 12:31:19 -0300

    second commit

--- R work.R
+++ R work.R
@@ -1,47 +1,45 @@
+#Importando a biblioteca "readr"
 library(readr)
-trabalhodeR <- read_csv("~/Minhas anotações/Int. a R para Ciência de dados/trabalhodeR.csv")
+#Importando os dados csv
+trabalhodeR <- read_csv("C:/Users/mateu/Desktop/Notas/Z - Trabalho de R/trabalhodeR.csv")
 View(trabalhodeR)
 
 
-###GrÃ¡ficos da aula
+#Importando a biblioteca "ggplot2"
 library(ggplot2)
 
 data(trabalhodeR)
 
+#Utilizando a funÃ§Ã£o "Summary" para fazer um resumo estatistico da base de dados.
 summary(trabalhodeR)
 
+#Utilizando a funÃ§Ã£o "nrow" para saber os numeros de linhas
+nrow(trabalhodeR)
 
-##1) 1Âº GrÃ¡fico (Dados: x = ExactMolWt, y = MolLogP, NumHAcceptors, NumHDonors, NumRotatableBonds, RingCount, TPSA)
+#Utilizando a funÃ§Ã£o "length" para obter/definir o comprimento da lista
+length(trabalhodeR)
+
+
+##Graficos com linha de tendencia 
+#(Dados: x = ExactMolWt, y = MolLogP, NumHAcceptors, NumHDonors, NumRotatableBonds, RingCount, TPSA)
+
 #(Dados: x = ExactMolWt, y = MolLogP (calculo da molecula))
-ggplot(data = trabalhodeR) + geom_point(mapping = aes(x = ExactMolWt, y = MolLogP))
-
 ggplot(data = trabalhodeR) + geom_point(mapping = aes(x = ExactMolWt, y = MolLogP)) + geom_smooth(mapping = aes(x = ExactMolWt, y = MolLogP))
-
-#Concentração de moléculas menores em encontro com o eXCTmOLwt e uma linha situada entre 0.5 e 5 MolLogP
+#Concentracao de moleculas menores em encontro com o ExactMolWt e uma linha situada entre -5 e 10 MolLogP
 
 #(Dados: x = ExactMolWt, y = NumHAcceptors)
-ggplot(data = trabalhodeR) + geom_point(mapping = aes(x = ExactMolWt, y = NumHAcceptors))
-
 ggplot(data = trabalhodeR) + geom_point(mapping = aes(x = ExactMolWt, y = NumHAcceptors)) + geom_smooth(mapping = aes(x = ExactMolWt, y = NumHAcceptors))
 
 #(Dados: x = ExactMolWt, y = NumHDonors)
-ggplot(data = trabalhodeR) + geom_point(mapping = aes(x = ExactMolWt, y = NumHDonors))
-
 ggplot(data = trabalhodeR) + geom_point(mapping = aes(x = ExactMolWt, y = NumHDonors)) + geom_smooth(mapping = aes(x = ExactMolWt, y = NumHDonors))
 
 #(Dados: x = ExactMolWt, y = NumRotatableBonds)
-ggplot(data = trabalhodeR) + geom_point(mapping = aes(x = ExactMolWt, y = NumRotatableBonds))
-
 ggplot(data = trabalhodeR) + geom_point(mapping = aes(x = ExactMolWt, y = NumRotatableBonds)) + geom_smooth(mapping = aes(x = ExactMolWt, y = NumRotatableBonds))
 
 #(Dados: x = ExactMolWt, y = RingCount)
-ggplot(data = trabalhodeR) + geom_point(mapping = aes(x = ExactMolWt, y = RingCount))
-
 ggplot(data = trabalhodeR) + geom_point(mapping = aes(x = ExactMolWt, y = RingCount)) + geom_smooth(mapping = aes(x = ExactMolWt, y = RingCount))
 
-#(Dados: x = ExactMolWt, y = TPSA (espaço da molecula))
-ggplot(data = trabalhodeR) + geom_point(mapping = aes(x = ExactMolWt, y = TPSA))
-
+#(Dados: x = ExactMolWt, y = TPSA (espa?o da molecula))
 ggplot(data = trabalhodeR) + geom_point(mapping = aes(x = ExactMolWt, y = TPSA)) + geom_smooth(mapping = aes(x = ExactMolWt, y = TPSA))
 
 
@@ -54,7 +52,7 @@ ggplot(data = trabalhodeR) + geom_point(mapping = aes(x = NumHAcceptors, y = Num
 
 
 
-##BOXplot
+##Fazendo o Boxplot
 #Boxplot do ExactMolWt e MolLogP
 ggplot(data = trabalhodeR, aes(y = MolLogP, x = ExactMolWt)) + geom_boxplot()
 
@@ -74,8 +72,42 @@ ggplot(data = trabalhodeR, aes(y = RingCount, x = ExactMolWt)) + geom_boxplot()
 ggplot(data = trabalhodeR, aes(y = TPSA, x = ExactMolWt)) + geom_boxplot()
 
 
+boxplot(trabalhodeR$ExactMolWt) +
+  legend(x = "topleft", legend = "ExactMolWt", bty = "n")
 
 
 
+##Quantidade que dados se repetem
+table(trabalhodeR$ExactMolWt)
+
+table(trabalhodeR$MolLogP)
+
+table(trabalhodeR$NumHAcceptors)
+
+table(trabalhodeR$NumHDonors)
+
+table(trabalhodeR$NumRotatableBonds)
+
+table(trabalhodeR$RingCount)
+
+table(trabalhodeR$TPSA)
+
+##Histograma
+hist(trabalhodeR$ExactMolWt)
+
+hist(trabalhodeR$MolLogP)
+
+hist(trabalhodeR$NumHAcceptors)
+
+hist(trabalhodeR$NumHDonors)
+
+hist(trabalhodeR$NumRotatableBonds)
+
+hist(trabalhodeR$RingCount)
+
+hist(trabalhodeR$TPSA)
 
 
+
+cor.test(trabalhodeR$ExactMolWt, trabalhodeR$RingCount)
+plot(trabalhodeR$ExactMolWt, trabalhodeR$RingCount)
